bcdc2f6997f067b7a9004d504f3b0f4317ba2884
Brenden Smit <brenden.smit@mnsu.edu>
2025-09-18 10:11:26 -0500

    Update Week3.py

--- Week 3/Week3.py
+++ Week 3/Week3.py
@@ -42,13 +42,26 @@ else:
 '''
 
 #Problem 5
+'''
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+num3 = int(input("enter third number: "))
 
-num1 = int(input("enter number: "))
-num2 = int(input("enter a second number: "))
-num3 = int(input("enter a third number: "))
-
-if num1 < num2:
+if num1 < num2 or num3:
+    if num2 < num3:
+        print(num1, num2, num3)
+    else:
+        print(num1, num3, num2)
+elif num2 < num1 or num3:
     if num1 < num3:
-        if num2 < num3:
-            print(num1, num2, num3)
-elif num
+        print(num2, num1, num3)
+    else:
+        print(num2, num3, num2)
+else:
+    if num1 < num2:
+        print(num3, num1, num2)
+    else:
+        print(num3, num2, num1)
+'''
+
+#Problem 6
