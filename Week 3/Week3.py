cce8a37cfb5e46c5e7753c07682263a31a23c820
Brenden Smit <brenden.smit@mnsu.edu>
2025-09-18 20:15:26 -0500

    Update Week3.py

--- Week 3/Week3.py
+++ Week 3/Week3.py
@@ -188,3 +188,11 @@ elif guess == "tails":
 else:
     print("Invalid Guess")
 '''
+
+#Problem 12
+
+num1 = int(input("enter a number: "))
+num2 = int(input("enter another number: "))
+num3 = int(input("enter another number: "))
+
+if 
