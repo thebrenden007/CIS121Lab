98d787a4f849531a0470ced1c47dc4b4dd8771a1
Brenden Smit <brenden.smit@mnsu.edu>
2025-09-23 13:57:24 -0500

    problem 3 and 4

--- Week 4/week4.py
+++ Week 4/week4.py
@@ -10,17 +10,30 @@ while (larger / 2 > smaller):
 print(counter)
 '''
 #Problem 2
-
+'''
 word = input("enter a word: ")
 pos = 1
 for pos in range(1, len(word), 2):
     print(word[pos])
-
+'''
 #Problem 3
+'''
 startnum = 37
 endnum = 1050
 num = 0
 
-while num > startnum and num < endnum:
+for num in range (37,1050 +1):
     if num % 2 == 0:
-        print(num += 1)
+        print(num)
+'''
+#Problem 4
+word = ''
+done = False
+while not done:
+    letter = input("enter a letter (or type done): ")
+    if letter != 'done':
+        word += letter
+        print(letter)
+    else:
+        done = True
+print(word)
