48980aaa534201253e42f4f0e4b90cf57ce70a1b
Brenden Smit <brenden.smit@mnsu.edu>
2025-10-24 10:52:33 -0500

    problem 2

--- Week 9/Week9.py
+++ Week 9/Week9.py
@@ -1,4 +1,5 @@
 #Problem 1
+'''
 def convert_knuts(knuts=450):
 	KNUTS_PER_SICKLE = 29
 	SICKLES_PER_GALLEON = 17
@@ -36,4 +37,28 @@ def convert_knuts(knuts=450):
 	
 	return output
 
-print(convert_knuts(650))
+print(convert_knuts(544))
+'''
+#Problem 2
+
+def highway_directions(highway_num):
+	if 1 < highway_num < 99:
+		if highway_num % 2 == 0:
+			return f"I-{highway_num} runs east/west"
+		else:
+			return f"I-{highway_num} runs north/south"
+
+	elif 100 < highway_num < 999:
+		service_highway = highway_num % 100
+
+		if 1 <= service_highway <= 99:
+			if service_highway % 2 == 0:
+				return f"I-{highway_num} runs east/west"
+			else:
+				return f"I-{highway_num} runs north/south"
+		else:
+			return f"I-{highway_num} is an invalid highway number"
+	else:
+		return f"I-{highway_num} is an invalid highway number"
+	
+print(highway_directions(353))
