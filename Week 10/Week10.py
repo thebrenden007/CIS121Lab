26bb477c60cb1389de32269f379a8064a5bf376f
Brenden Smit <brenden.smit@mnsu.edu>
2025-10-31 10:44:24 -0500

    Problem 15

--- Week 10/Week10.py
+++ Week 10/Week10.py
@@ -38,7 +38,9 @@ class Candy:
             return "WOW!!!"
     def __str__(self):
         return f"{self.name} its {self.flavor} cost {self.price} and is of size and shape {self.size}, {self.shape} {self.reaction()}"
-    
+
+bagofcandies = []
+
 candy1 = Candy()
 candy1.set_name("KitKat")
 candy1.set_shape("Rectangle")
@@ -46,3 +48,35 @@ candy1.set_size(10)
 candy1.set_price(499.99)
 candy1.set_flavor("Chocolate")
 print(candy1)
+
+bagofcandies.append(candy1)
+
+for candy in bagofcandies:
+    print(candy)
+
+#Problem 15
+
+class Recipe:
+    def __init__(self):
+        self.name = "unknown"
+        self.cooktime = 0
+    def get_name(self):
+        return self.name
+    def set_name(self, value):
+        self.name = value
+    def get_cooktime(self):
+        return self.cooktime
+    def set_cooktime(self, value):
+        self.cooktime = value
+    def is_quick_meal(self):
+        if self.cooktime < 30:
+            return True
+        else:
+            return False
+    def __str__(self):
+        return f"The recipe is for {self.name} and takes {self.cooktime} minutes to cook"
+    
+recipe1 = Recipe()
+recipe1.set_name("Alfredo")
+recipe1.set_cooktime(35)
+print(recipe1)
