7b6a098951b479cb7fa9273bb99b9a17b56d699a
Brenden Smit <brenden.smit@mnsu.edu>
2025-11-07 08:21:50 -0600

    Update Week10.py

--- Week 10/Week10.py
+++ Week 10/Week10.py
@@ -1,3 +1,4 @@
+'''
 class Candy:
     def __init__(self):
         self.name = "unknown"
@@ -53,9 +54,70 @@ bagofcandies.append(candy1)
 
 for candy in bagofcandies:
     print(candy)
+'''
+#Problem 1
+'''
+class Product:
+    def __init__(self):
+        self.name = 'unknown'
+        self.price = 0
+        self.quantity = 0
+    def get_name(self):
+        return self.name
+    def set_name(self, value):
+        self.name = value
+    def get_price(self):
+        return self.price
+    def set_price(self, value):
+        self.price = value
+    def get_quantity(self):
+        return self.quantity
+    def set_quantity(self, value):
+        self.quantity = value
+    def __str__(self):
+        return f'The product is a {self.name}, it cost ${self.price}, and there are {self.quantity} available.'
 
+product1 = Product()
+product1.set_name('iPad')
+product1.set_price(350)
+product1.set_quantity(5)
+print(product1)
+'''
+#Problem 6
+
+class Student:
+    def __init__(self, name, major, GPA):
+        self.name = name
+        self.major = major
+        self.GPA = GPA
+    def get_name(self):
+        return self.name
+    def set_name(self, value):
+        self.name = value
+    def get_major(self):
+        return self.major
+    def set_major(self, value):
+        self.major = value
+    def get_GPA(self):
+        return self.GPA
+    def set_GPA(self, value):
+        self.GPA = value
+    def introduce(self):
+        return f'Hi, I am {self.name}. I am studying {self.major}.'
+    def studying(self):
+        old_GPA = self.GPA
+        self.GPA += 0.2
+        if self.GPA > 4.0:
+            self.GPA = 4.0
+        return f'I am hitting the books! My GPA increased from {old_GPA} to {self.GPA}'
+student1 = Student('brenden', 'CIT', 3.5)
+print(student1.introduce())
+print(student1.get_GPA())
+print(student1.studying())
+
+#print(student1.studying())
 #Problem 15
-
+'''
 class Recipe:
     def __init__(self):
         self.name = "unknown"
@@ -80,3 +142,4 @@ recipe1 = Recipe()
 recipe1.set_name("Alfredo")
 recipe1.set_cooktime(35)
 print(recipe1)
+'''
