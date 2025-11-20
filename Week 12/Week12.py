4710e7742751a05a277d4c1fa7f3c3a99c141b8c
Brenden Smit <brenden.smit@mnsu.edu>
2025-11-20 09:35:41 -0600

    Update Week12.py

--- Week 12/Week12.py
+++ Week 12/Week12.py
@@ -1,4 +1,5 @@
 #Problem 9
+'''
 class Playlist:
     def __init__(self, name = "New Playlist", songs = []):
         self.name = name
@@ -17,3 +18,20 @@ p2 = Playlist("Country", ["Last Night", "What I Want"])
 p1.add_song("Mistle Toe")
 new_p = p1 + p2
 print(new_p)
+'''
+#Problem 10
+
+class ShoppingCart:
+    def __init__(self, items = {}):
+        self.items = items
+    def add_item(self, item):
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
+p1 = ShoppingCart({"tea" : 1, "energy drink" : 2})
+p1.add_item({"sandwich" : 2})
+
+p2 = ShoppingCart({"energy drink" : 3, "hat" : 1})
+print(p2)
