fa93cd64b0d261b5a7755c091e54c041590a236a
Brenden Smit <brenden.smit@mnsu.edu>
2025-12-05 07:40:29 -0600

    problem 7

--- Week 14/Week14.py
+++ Week 14/Week14.py
@@ -33,3 +33,18 @@ except ValueError:
 except KeyError:
     print("Student not found")
 '''
+#Problem 7
+'''
+try:
+    num1 = int(input("enter a number: "))
+    num2 = int(input("enter another number: "))
+    difference = num1 - num2
+    ratio = num1 / num2
+    print(f'Difference: {difference}, Ratio: {ratio}')
+except ValueError:
+    print("Invalid Input")
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except OverflowError:
+    print("Result too large")
+'''
